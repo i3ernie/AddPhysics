1794924f83e5de5bd2c699bb9aab21dd528c2260
bernie <mail@javaner.de>
2019-05-13 05:44:04 +0200

    add: worker AddPhysics_worker

---
+++ src/AddPhysics_worker.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+importScripts( "./worker/globals.js" );
+importScripts( "./worker/init.js" );
+importScripts( "./worker/world.js" );
+importScripts( "./worker/Shapes.js" );
+importScripts( "./worker/vehicle.js" );
+
+
+importScripts( "./worker/Constraints.js" );
+importScripts( "./worker/DOFConstraint.js" );
+importScripts( "./worker/HingeConstraint.js" );
+importScripts( "./worker/SliderConstraint.js" );
+importScripts( "./worker/ConetwistConstraint.js" );
+importScripts( "./worker/PointConstraint.js" );
