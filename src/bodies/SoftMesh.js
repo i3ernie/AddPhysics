cba9585c890868e87ab32411f0b6b3b746246d2d
bernie <mail@javaner.de>
2019-05-13 05:53:48 +0200

    update: add mapIndices

--- src/bodies/SoftMesh.js
+++ src/bodies/SoftMesh.js
@@ -26,6 +26,30 @@ function processGeometry( bufGeometry ) {
         mapIndices( bufGeometry, indexedBufferGeom );
 }
 
+    function mapIndices( bufGeometry, indexedBufferGeom ) {
+            // Creates ammoVertices, ammoIndices and ammoIndexAssociation in bufGeometry
+            var vertices = bufGeometry.attributes.position.array;
+            var idxVertices = indexedBufferGeom.attributes.position.array;
+            var indices = indexedBufferGeom.index.array;
+            var numIdxVertices = idxVertices.length / 3;
+            var numVertices = vertices.length / 3;
+            bufGeometry.ammoVertices = idxVertices;
+            bufGeometry.ammoIndices = indices;
+            bufGeometry.ammoIndexAssociation = [];
+            for ( var i = 0; i < numIdxVertices; i ++ ) {
+                    var association = [];
+                    bufGeometry.ammoIndexAssociation.push( association );
+                    var i3 = i * 3;
+                    for ( var j = 0; j < numVertices; j ++ ) {
+                            var j3 = j * 3;
+                            if ( isEqual( idxVertices[ i3 ], idxVertices[ i3 + 1 ], idxVertices[ i3 + 2 ],
+                                    vertices[ j3 ], vertices[ j3 + 1 ], vertices[ j3 + 2 ] ) ) {
+                                    association.push( j3 );
+                            }
+                    }
+            }
+    }
+
 let SoftMesh = function( geometry, material, mass, pessure ){
     
     Mesh.call( this, geometry, material, mass );
